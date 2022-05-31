7c58c3afcd78f130e90d9ffbd6cd472acf89ac3d
Dubai <poopyface477@gmail.com>
2022-05-31 22:29:07 +0400

    [+] Added LunarClient Staff Mode Disable CMD

--- src/main/java/xyz/refinedev/lunar/commands/LunarStaffCommand.java
+++ src/main/java/xyz/refinedev/lunar/commands/LunarStaffCommand.java
@@ -13,11 +13,25 @@ public class LunarStaffCommand {
     public void onLunarStaffModeCommand(@Sender Player player, @Optional @Name("target") Player target) {
         if (target == null) {
             LunarClientAPI.getInstance().giveAllStaffModules(player);
-            player.sendMessage(Utils.parsePapi(player, Locale.LUNAR_STAFF_COMMAND_PLAYER.messageFormat()));
+            player.sendMessage(Utils.parsePapi(player, Locale.LUNAR_STAFF_COMMAND_ENABLE_PLAYER.messageFormat()));
         } else {
             LunarClientAPI.getInstance().giveAllStaffModules(target);
-            player.sendMessage(Utils.parsePapi(player, Locale.LUNAR_STAFF_COMMAND_TARGET.messageFormat()).replace("<target>", target.getDisplayName()));
-            target.sendMessage(Utils.parsePapi(target, Locale.LUNAR_STAFF_COMMAND_TO_TARGET.messageFormat()).replace("<player>", player.getDisplayName()));
+            player.sendMessage(Utils.parsePapi(player, Locale.LUNAR_STAFF_COMMAND_ENABLE_TARGET.messageFormat()).replace("<target>", target.getDisplayName()));
+            target.sendMessage(Utils.parsePapi(target, Locale.LUNAR_STAFF_COMMAND_ENABLE_TO_TARGET.messageFormat()).replace("<player>", player.getDisplayName()));
+        }
+    }
+
+    @Command(value = {"lunarstaffmode disable", "lcstaffmode disable", "lunarstaffmod disable", "lunarstaffmods disable", "lsm disable", "lsm remove"})
+    @Permission(value = "lunar.staff")
+    public void onLunarStaffModeRemoveCommand(@Sender Player player, @Optional @Name("target") Player target) {
+
+        if (target == null) {
+            LunarClientAPI.getInstance().disableAllStaffModules(player);
+            player.sendMessage(Utils.parsePapi(player, Locale.LUNAR_STAFF_COMMAND_DISABLE_PLAYER.messageFormat()));
+        } else {
+            LunarClientAPI.getInstance().disableAllStaffModules(target);
+            player.sendMessage(Utils.parsePapi(player, Locale.LUNAR_STAFF_COMMAND_DISABLE_TARGET.messageFormat()).replace("<target>", target.getDisplayName()));
+            target.sendMessage(Utils.parsePapi(target, Locale.LUNAR_STAFF_COMMAND_DISABLE_TO_TARGET.messageFormat()).replace("<player>", player.getDisplayName()));
         }
     }
 }
